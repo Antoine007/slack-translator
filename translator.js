2cc21d7a4cddd39fb117160cfc620b532abdcfcc
antoine magny <mr.magny+github@gmail.com>
2015-12-03 13:43:51 +0100

    returns user name and channel_name

--- translator.js
+++ translator.js
@@ -1,40 +1,29 @@
 var request = require('request');
 
 module.exports = function (req, res, next) {
-  var text;
+
   var botPayload = {};
-  var user_name = req.body.user_name;
+
+  var text;
+  var channel_name = req.body.channel_name;
+  var user_name    = req.body.user_name;
+
   text = req.body.text;
-  botPayload.text = translate(text);
-  // botPayload.text = req.body.text;
+
+  botPayload.text      = translate(text);
   botPayload.user_name = user_name;
-  // botPayload.channel = req.body.channel_id;
+  botPayload.channel_name = channel_name;
 
-
-  if (user_name !== 'translate-this') {
+  if (channel_name !== 'translate-this') {
     return res.status(200).send(botPayload);
   } else {
     return res.status(200).end();
   }
 
-
-
-  // send(botPayload) {
-  //   if (error) {
-  //     return next(error);
-  //   } else if (status !== 200) {
-  //     // inform user that our Incoming WebHook failed
-  //     return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-  //   } else {
-  //     return res.status(200).end();
-  //   }
-  // });
-
-
 };
 
 function translate (text) {
-  return text + "_is what you meant!_ :flag-uk: please! Thanks!"
+  return text + " _is what you meant!_ :flag-uk: please! Thanks!    "
 }
 
 function send (payload) {
