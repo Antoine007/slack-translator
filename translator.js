47d1c21066da95f93017a6d24a18e0ceea30312d
antoine magny <mr.magny+github@gmail.com>
2015-12-03 13:33:07 +0100

    curl works

--- translator.js
+++ translator.js
@@ -3,41 +3,47 @@ var request = require('request');
 module.exports = function (req, res, next) {
   var text;
   var botPayload = {};
-  botPayload.text = translate(req.body.text);
-  botPayload.username = 'translate-this';
-  botPayload.channel = req.body.channel_id;
+  var user_name = req.body.user_name;
+  text = req.body.text;
+  botPayload.text = translate(text);
+  // botPayload.text = req.body.text;
+  botPayload.user_name = user_name;
+  // botPayload.channel = req.body.channel_id;
 
-  send(botPayload, function (error, status, body) {
-    if (error) {
-      return next(error);
-    } else if (status !== 200) {
-      // inform user that our Incoming WebHook failed
-      return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-    } else {
-      return res.status(200).end();
-    }
-  });
+
+  if (user_name !== 'translate-this') {
+    return res.status(200).send(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+
+
+
+  // send(botPayload) {
+  //   if (error) {
+  //     return next(error);
+  //   } else if (status !== 200) {
+  //     // inform user that our Incoming WebHook failed
+  //     return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+  //   } else {
+  //     return res.status(200).end();
+  //   }
+  // });
 
 
 };
 
 function translate (text) {
-  text = text
+  return text + "_is what you meant!_ :flag-uk: please! Thanks!"
 }
 
-function send (payload, callback) {
+function send (payload) {
   var path = process.env.INCOMING_WEBHOOK_PATH;
-  var uri = 'https://hooks.slack.com/services' + path;
+  var uri = 'https://hooks.slack.com/services/' + path;
 
   request({
     uri: uri,
     method: 'POST',
     body: JSON.stringify(payload)
-  }, function (error, response, body) {
-    if (error) {
-      return callback(error);
-    }
-
-    callback(null, response.statusCode, body);
   });
 }
